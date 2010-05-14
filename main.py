b2fad020aab02b73400bc99470e53773ae971867
Scott Mansell <phiren@gmail.com>
2010-05-15 10:40:39 +1200

    Window is now fullscreen, press escape to exit.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import pyglet
+from pyglet.window import key
 
-window = pyglet.window.Window()
+window = pyglet.window.Window(fullscreen=True)
 planet_image = pyglet.resource.image('images/planet.png')
 planet = pyglet.sprite.Sprite(planet_image)
 
@@ -10,6 +11,12 @@ planet = pyglet.sprite.Sprite(planet_image)
 def on_draw():
 	window.clear()
 	planet.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+	print symbol, modifiers
+	if symbol == key.ESCAPE:
+		pyglet.app.exit()
 
 pyglet.app.run()
 
