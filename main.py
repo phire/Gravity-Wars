586c2a6ffde4acf1455d2906eb01c86c833d2891
Scott Mansell <phiren@gmail.com>
2010-05-15 10:48:28 +1200

    Scaled down planet, moved to center

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from pyglet.window import key
 window = pyglet.window.Window(fullscreen=True)
 planet_image = pyglet.resource.image('images/planet.png')
 planet = pyglet.sprite.Sprite(planet_image)
+planet.scale = 0.1
+planet.x = window.width // 2 - planet.width // 2
+planet.y = window.height // 2 - planet.height // 2
 
 @window.event
 def on_draw():
